bc0019733a66a99155e9fde945180a32bb46b6ce
hassan11196 <hassan11196@hotmail.com>
2019-09-10 23:46:56 +0500

    Shifted To AbstractUser, Added Models for Teacher, Student, Semester.

---
+++ QRSMS/Dumps/SQL COURSE INSERTS.sql
@@ -0,0 +1,96 @@
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'applied physics','NS101','AP');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'calculus and analytical geometry','MT119','Cal');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'english composition and comprehension','SS150','Eng');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'english composition and comprehension - lab','SL150','Eng-Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'introduction to information and communication technologies','CS117','ICT-Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'islamic and religious studies','SS111','IRS');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'professional ethics (for non-muslims)','SS203','Ethics');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'programming fundamentals','CS118','PF');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'programming fundamentals lab','CL118','PF-Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'calculus-ii','MT115','Cal-||');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'comp. organization & assembly language lab','EL213','COAL Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'comp. organization & assembly language.','EE213','COAL');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'computer programming','CS103','CP');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'computer programming lab','CL103','CP Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'data structures','CS201','DS');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'data structures lab','CL201','DS Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'digital logic design','EE227','DLD');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'digital logic design lab','EL227','DLD Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'discrete structures','CS211','Discrete');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'english composition','SS122','Eng comp');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'finance and accounting','MG103','FA');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'linear algebra','MT104','LA');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'marketing management','MG220','MM');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'psychology','SS118','Psych');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'communication for managers','MG335','CM');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'computer architecture','EE204','CA');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'database systems','CS203','DB');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'database systems lab','CL203','DB-Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'design & analysis of algorithms','CS302','Algo');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'directed reading economics','MG315','DRE');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'fundamentals of management','MG223','FM');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'object oriented analysis & design','CS309','OOAD');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'object oriented analysis & design lab','CL309','OOAD-Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'operating systems','CS205','OS');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'operating systems lab','CL205','OS Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'operations research','MT303','OR');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'probability & statistics','MT206','Prob');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'theory of automata','CS301','TOA');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'computer networks','CS307','CN');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'computer networks lab','CL307','CN-Gr1 Lab');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'data sciences','CS481','DSci');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'human computer interaction','CS422','HCI');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'information processing techniques','CS423','IPT');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'information security','CS446','IS');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'introduction to software project management','CS450','ISPM');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'introdution to cloud computing','CS499','ICC');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'professional issues in it','CS449','PIT');
+INSERT INTO initial_course (course_name,course_code,course_short) VALUES (
+'software engineering','CS303','SE');
