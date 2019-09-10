6985be68739c36f5b70832ed114a282c498362cd
hassan11196 <hassan11196@hotmail.com>
2019-09-10 23:50:41 +0500

    Shifted to AbstractUser. Created models for Teacher, Student, Semester

--- QRSMS/initial/admin.py
+++ QRSMS/initial/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Teacher, Faculty, Student, Semester
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Teacher)
+admin.site.register(Faculty)
+admin.site.register(Student)
+admin.site.register(Semester)
