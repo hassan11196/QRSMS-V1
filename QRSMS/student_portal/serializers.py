431d09ddb8cb5b74b314284f15ced156b0ed3be4
hassan11196 <hassan11196@hotmail.com>
2020-01-06 03:27:38 +0500

    Add Teacher Attendance View

--- QRSMS/student_portal/serializers.py
+++ QRSMS/student_portal/serializers.py
@@ -2,6 +2,12 @@
 from rest_framework import serializers
 from .models import Student
 
+
+class StudentSerializerNameAndUid(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        depth = 2
 class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
