4a2606120f96349ea98f8108f3d751b66c2dc958
hassan11196 <hassan11196@hotmail.com>
2020-01-06 19:05:54 +0500

    Wrapped Serialzers and Teacher Attendnace API

--- QRSMS/student_portal/serializers.py
+++ QRSMS/student_portal/serializers.py
@@ -3,18 +3,33 @@ from rest_framework import serializers
 from .models import Student
 from actor.models import User
 
+
+def WrapperStudentSerializer(*args):
+    class StudentSerializerCustom(serializers.ModelSerializer):
+        class Meta:
+            model = Student
+            fields = args
+
+    return StudentSerializerCustom()
+
 class UserSerializerOnlyName(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'last_name')
-class StudentSerializerNameAndUid(serializers.ModelSerializer):
+class StudentSerializerAllData(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
         depth = 2
+
 class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+class StudentSerializerOnlyNameAndUid(serializers.ModelSerializer):
     
     user = UserSerializerOnlyName()
+
     class Meta:
         model = Student
         fields = (
